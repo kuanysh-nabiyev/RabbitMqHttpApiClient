1af362bd59bbb36a27d4b8ceeb687fa4823b2df6
kuanysh-nabiyev <nabi.kuanysh@gmail.com>
2017-04-24 12:01:53 +0600

    comment was added

--- RabbitMqHttpApiClient.ConsoleApp/API/RabbitMqApi.Consumer.cs
+++ RabbitMqHttpApiClient.ConsoleApp/API/RabbitMqApi.Consumer.cs
@@ -7,11 +7,17 @@ namespace RabbitMqHttpApiClient.ConsoleApp.API
 {
     public partial class RabbitMqApi//.Consumer
     {
+        /// <summary>
+        /// A list of all consumers.
+        /// </summary>
         public async Task<IEnumerable<Consumer>> GetConsumers()
         {
             return await DoGetCall<IEnumerable<Consumer>>("api/consumers");
         }
 
+        /// <summary>
+        /// A list of all consumers in a given virtual host.
+        /// </summary>
         public async Task<IEnumerable<Consumer>> GetConsumersByVhost(string virtualHost)
         {
             return await DoGetCall<IEnumerable<Consumer>>($"api/consumers/{virtualHost.Encode()}");
